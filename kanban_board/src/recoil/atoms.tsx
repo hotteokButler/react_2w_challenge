a35d6de6fb9a14259e3a70e383ab877a7d9e51ed
hotteokButler <ygs12567@gmail.com>
2024-04-08 17:07:50 +0900

    Fix Expectation Violation: Duplicate atom key Error
    -recoil 중복 키 값 오류
    
    -> RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED를 false로 설정
      (.env file에서 설정 가능하나 vite에서는 위의 방법으로 적용해야함 / 따로 파일 분리하지 않고 recoil에서 사용되는 file에 선언)
    -> 키값을 다르게 주기 (uuid활용)
    
    -> 파일 분리해서 첫번째 방법 적용 안된거라 커밋 후 수정 예정

--- kanban_board/src/recoil/atoms.tsx
+++ kanban_board/src/recoil/atoms.tsx
@@ -1,9 +1,10 @@
 import { DefaultValue, atom, selector } from 'recoil';
+import { v4 as uuidv4 } from 'uuid';
 
 export const listState = atom<{
-  [key: string]: string[];
+  [key: string]: string[] | [];
 }>({
-  key: 'lists',
+  key: `lists/${uuidv4()}`,
   default: {
     plan: ['1', '2', '3', '4', '5'],
     progress: [],
