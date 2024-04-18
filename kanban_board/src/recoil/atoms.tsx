e9934d382170de8e4825ae363cae8aa77dab3019
hotteokButler <ygs12567@gmail.com>
2024-04-18 11:49:20 +0900

    style 수정

--- kanban_board/src/recoil/atoms.tsx
+++ kanban_board/src/recoil/atoms.tsx
@@ -8,9 +8,8 @@ export interface IListState {
 export const listState = atom<IListState>({
   key: `lists/${uuidv4()}`,
   default: {
-    plan: ['1', '2', '3', '4', '5'],
-    progress: ['one', 'two', 'three'],
+    plan: ['1', '2'],
+    progress: ['one', 'two'],
     done: ['done1', 'done2'],
   },
 });
-
